b2ac03367994489d219ea1b79230d2998613838a
csaez30 <csaez2007@gmail.com>
2018-09-15 11:26:02 -0300

    clase empleado, por cris

--- src/ejerciciofer/Empleado.java
+++ src/ejerciciofer/Empleado.java
@@ -3,12 +3,67 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejerciciofer;
+package ejemplofer;
 
 /**
  *
- * @author juanj
+ * @author cris
  */
-class Empleado {
+public class Empleado {
+    String nombre;
+    String apellido;
+    String documento;
+    String categoria;
+    Double sueldo;
+
+    public Empleado(String nombre, String apellido, String documento, String categoria, Double sueldo) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.documento = documento;
+        this.categoria = categoria;
+        this.sueldo = sueldo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(Double sueldo) {
+        this.sueldo = sueldo;
+    }
     
+    
+            
 }
